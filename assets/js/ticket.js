68e726581976285eb41179e326df096c8f72cb25
lucasperea <lukeperea@gmail.com>
2016-09-08 13:28:22 -0700

    another

--- assets/js/ticket.js
+++ assets/js/ticket.js
@@ -1,12 +1,25 @@
-	//ticket swipe 
- 
-	$(function() {
-        $('.ticket').ready(function(){ 
-             $('#001').on("swipe", function() {
-	              $(this).toggle(300);
-                    });
-	   
-                });
-            });
+//variable stuff
+
+
+
+//ticket swipe 
+
+$(function() {
+    $('.ticket').ready(function() {
+
+
+        //ticket 1
+        $('#001').on("swipe", function() {
+            $(this).hide(200);
+        });
+
+        //ticket 2
+        $('#002').on("swipe", function() {
+            $(this).hide(200);
+        });
+    });
+});
+
+
 
 //jq playground
