cecc0d3438732f1559078c091a1303dd0836d5b1
lucasperea <lukeperea@gmail.com>
2016-09-08 11:51:41 -0700

    add JS

---
+++ assets/js/ticket.js
@@ -0,0 +1,12 @@
+	//ticket swipe 
+ 
+	$(function() {
+        $('.ticket').ready(function(){ 
+             $('#001').on("swipe", function() {
+	              $(this).toggle(300);
+                    });
+	   
+                });
+            });
+
+//jq playground
