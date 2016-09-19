60fbcaaf63233064bc68d3c5ac049137af4163d9
lucasperea <lukeperea@gmail.com>
2016-09-19 10:11:40 -0700

    update [x] remove jquery ui-a background color [x] remove 'next ticket' option from desktop view

--- assets/js/ticket.js
+++ assets/js/ticket.js
@@ -1,7 +1,9 @@
 //variable stuff
 
-
-
+if(document.getElementById("#001").style.display != 'block')
+{
+    document.getElementById(id).style.display = 'none';
+};
 //ticket swipe 
 
 $(function() {
@@ -18,12 +20,25 @@ $(function() {
                      $(this).hide(200);
        
         });
+         //ticket 3
+                $('#003').on("swipe", function() {
+                     $(this).hide(200);
+       
+        });
+         //ticket 4
+                $('#004').on("swipe", function() {
+                     $(this).hide(200);
+       
+        });
+         //ticket 5
+                $('#005').on("swipe", function() {
+                     $(this).hide(200);
+       
+        });
+         //ticket 6
+                $('#006').on("swipe", function() {
+                     $(this).hide(200);
+       
+        });
     });
 });
-
-
-$(function(){
-    $("body").removeClass('ui-overlay-a');
-});
-
-//jq playground
